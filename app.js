377904096a8441f5c66c2751c95e9333b401bd8e
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-07 10:32:30 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
 const { customErrors, psqlErrors, serverErrors } = require("./errors");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
